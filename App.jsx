f2641204ee8df8292f5c148c7ab8e49aeb4099ed
Ahmad Nurhidayat <nur26691@gmail.com>
2024-06-20 20:26:19 +0700

    refactor handleSearch, add status render

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ActivityIndicator, Text } from "react-native";
 import WeatherSearch from "./components/WeatherSearch";
 import WeatherInfo from "./components/WeatherInfo";
 import axios from "axios";
@@ -8,36 +8,82 @@ import { useState } from "react";
 
 export default function App() {
   const [weatherData, setWeatherData] = useState();
+  const [status, setStatus] = useState();
 
-  function handleSearch(location) {
-    axios
-      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
-      .then((res) => {
-        const data = res.data;
-        data.visibility /= 1000;
-        data.visibility = data.visibility.toFixed(2);
-        data.main.temp -= 273.15;
-        data.main.temp = data.main.temp.toFixed(2);
+  // handle seacrh with promise
+  // function handleSearch(location) {
+  //   if (location === "") return;
 
-        setWeatherData(data);
-      })
-      .catch((err) => console.error("gagal cur", err));
-  }
+  //   setStatus("loading");
+  //   axios
+  //     .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+  //     .then((res) => {
+  //       const data = res.data;
+  //       data.visibility /= 1000;
+  //       data.visibility = data.visibility.toFixed(2);
+  //       data.main.temp -= 273.15;
+  //       data.main.temp = data.main.temp.toFixed(2);
 
-  console.log(weatherData);
+  //       setWeatherData(data);
+  //       setStatus("success");
+  //     })
+  //     .catch((err) => {
+  //       setStatus("error");
+  //       console.error("gagal cur", err);
+  //     });
+  // }
+
+  // handle seacrh with promise
+  const handleSearch = async (location) => {
+    setStatus("loading");
+    try {
+      const res = await axios.get(`${BASE_URL}?q=${location}&appid=${API_KEY}`);
+      const data = res.data;
+      data.visibility /= 1000;
+      data.visibility = data.visibility.toFixed(2);
+      data.main.temp -= 273.15;
+      data.main.temp = data.main.temp.toFixed(2);
+
+      setWeatherData(data);
+    } catch (error) {
+      setStatus("error");
+      console.error("error catch:", error);
+    } finally {
+      setStatus("success");
+    }
+  };
+
+  const renderComponent = () => {
+    switch (status) {
+      case "loading":
+        return <ActivityIndicator size={"large"} />;
+      case "success":
+        return (
+          <WeatherInfo
+            name={weatherData?.name}
+            temp={weatherData?.main?.temp}
+            weatherDesc={weatherData?.weather[0]?.description}
+            icon={weatherData?.weather[0]?.icon}
+            visibility={weatherData?.visibility}
+            windSpeed={weatherData?.wind.speed}
+          />
+        );
+      case "error":
+        return (
+          <Text style={{ marginTop: 42 }}>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <WeatherSearch searchWeather={handleSearch} />
-      <WeatherInfo
-        name={weatherData?.name}
-        temp={weatherData?.main?.temp}
-        weatherDesc={weatherData?.weather[0]?.description}
-        icon={weatherData?.weather[0]?.icon}
-        visibility={weatherData?.visibility}
-        windSpeed={weatherData?.wind.speed}
-      />
+      {renderComponent()}
     </View>
   );
 }
